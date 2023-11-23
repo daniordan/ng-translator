410d7fd34455ed112b0237df7fc184643e533de9
mariusdanbmd <34275284+mariusdanbmd@users.noreply.github.com>
2023-11-23 15:30:23 +0200

    Initial implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { TranslateService } from './translate.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ng-translator';
+  // text: string;
+  // translatedText: string;
+  text = '';
+  translatedText = '';
+
+  constructor(private translateService: TranslateService) {}
+
+  onTextInput(event: Event) {
+    this.text = (<HTMLInputElement>event.target).value;
+  }
+
+  submit() {
+    this.translateService.translate(this.text).subscribe((result) => {
+      this.translatedText = result;
+    });
+  }
 }
